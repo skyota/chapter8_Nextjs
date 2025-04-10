046ec02219e830ccb4a461a186446b97c228f998
skyota <kyotas3007@gmail.com>
2025-04-10 20:24:50 +0900

    useFetchの返り値の修正

--- src/app/_hooks/useFetch.ts
+++ src/app/_hooks/useFetch.ts
@@ -2,9 +2,6 @@ import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
-const useFetch = <T,>(url: string) => {
-  const { data, error, isLoading } = useSWR<T>(url, fetcher);
-  return { data, error, isLoading };
-};
+const useFetch = <T,>(url: string) => useSWR<T>(url, fetcher);
 
 export default useFetch;
