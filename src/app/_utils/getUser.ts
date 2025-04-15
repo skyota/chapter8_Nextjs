67656de76cbde6ea0d088ff70f4e7d58f065d3f2
skyota <kyotas3007@gmail.com>
2025-04-15 19:05:24 +0900

    getUserの返り値の修正

--- src/app/_utils/getUser.ts
+++ src/app/_utils/getUser.ts
@@ -3,6 +3,5 @@ import { NextRequest } from "next/server";
 
 export const getUser = async (request: NextRequest) => {
   const token = request.headers.get("Authorization") ?? "";
-  const { data, error } = await supabase.auth.getUser(token);
-  return { data, error };
+  return await supabase.auth.getUser(token);
 };
